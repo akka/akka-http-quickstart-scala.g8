106c190faad8d990fed4784ed25d2eb18875495a
Jeremy Bratton <jeremy.bratton@asynchrony.com>
2017-08-16 12:12:22 -0500

    Escape `$` in Scala source
    Fixes https://github.com/akka/akka-http-quickstart-scala.g8/issues/15

--- src/main/g8/src/main/scala/com/lightbend/akka/http/sample/UserRegistryActor.scala
+++ src/main/g8/src/main/scala/com/lightbend/akka/http/sample/UserRegistryActor.scala
@@ -27,11 +27,11 @@ class UserRegistryActor extends Actor with ActorLogging {
       sender() ! Users(users.toSeq)
     case CreateUser(user) =>
       users += user
-      sender() ! ActionPerformed(s"User ${user.name} created.")
+      sender() ! ActionPerformed(s"User \${user.name} created.")
     case GetUser(name) =>
       sender() ! users.find(_.name == name)
     case DeleteUser(name) =>
       users.find(_.name == name) foreach { user => users -= user }
-      sender() ! ActionPerformed(s"User ${name} deleted.")
+      sender() ! ActionPerformed(s"User \${name} deleted.")
   }
 }
