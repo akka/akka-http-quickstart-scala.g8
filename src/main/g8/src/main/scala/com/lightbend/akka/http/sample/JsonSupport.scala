0431dcce806210cafd2cd0e05b39edc47b535daf
Roman Filonenko <shkoder@users.noreply.github.com>
2017-08-18 15:08:19 +0200

    remove unused import

--- src/main/g8/src/main/scala/com/lightbend/akka/http/sample/JsonSupport.scala
+++ src/main/g8/src/main/scala/com/lightbend/akka/http/sample/JsonSupport.scala
@@ -2,7 +2,7 @@ package com.lightbend.akka.http.sample
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.lightbend.akka.http.sample.UserRegistryActor.ActionPerformed
-import spray.json.{ DefaultJsonProtocol, RootJsonFormat }
+import spray.json.DefaultJsonProtocol
 
 trait JsonSupport extends SprayJsonSupport {
   import DefaultJsonProtocol._ // import the default encoders for primitive types (Int, String, Lists etc)
