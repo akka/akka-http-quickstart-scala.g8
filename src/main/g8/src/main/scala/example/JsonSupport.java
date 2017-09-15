1ed63d3c909ed3ab4b3333fabd657690801f5f2c
Andrea Raspitzu <a.raspitzu@gmail.com>
2017-09-15 21:55:58 +0200

    Reorganize files

--- src/main/g8/src/main/scala/example/JsonSupport.java
+++ src/main/g8/src/main/scala/example/JsonSupport.java
@@ -1,9 +1,3 @@
-package com.lightbend.akka.http.sample
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.lightbend.akka.http.sample.UserRegistryActor.ActionPerformed
-import spray.json.DefaultJsonProtocol
-
 trait JsonSupport extends SprayJsonSupport {
   // import the default encoders for primitive types (Int, String, Lists etc)
   import DefaultJsonProtocol._
