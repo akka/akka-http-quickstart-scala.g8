ba7f5c0f0ef76af99793c1a9de2ec0a0d7c3967c
László van den Hoek <laszlo.vandenhoek@gmail.com>
2018-09-14 13:52:03 +0200

    Reflect asynchronous nature of bindAndHandle() (#57)

--- src/main/g8/src/main/scala/$package$/QuickstartServer.scala
+++ src/main/g8/src/main/scala/$package$/QuickstartServer.scala
@@ -1,8 +1,9 @@
 package $package$
 
 //#quick-start-server
-import scala.concurrent.Await
+import scala.concurrent.{ Await, ExecutionContext, Future }
 import scala.concurrent.duration.Duration
+import scala.util.{ Failure, Success }
 
 import akka.actor.{ ActorRef, ActorSystem }
 import akka.http.scaladsl.Http
@@ -17,6 +18,7 @@ object QuickstartServer extends App with UserRoutes {
   //#server-bootstrapping
   implicit val system: ActorSystem = ActorSystem("helloAkkaHttpServer")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContext = system.dispatcher
   //#server-bootstrapping
 
   val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
@@ -27,9 +29,16 @@ object QuickstartServer extends App with UserRoutes {
   //#main-class
 
   //#http-server
-  Http().bindAndHandle(routes, "localhost", 8080)
+  val serverBinding: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
 
-  println(s"Server online at http://localhost:8080/")
+  serverBinding.onComplete {
+    case Success(bound) =>
+      println(s"Server online at http://\${bound.localAddress.getHostString}:\${bound.localAddress.getPort}/")
+    case Failure(e) =>
+      Console.err.println(s"Server could not start!")
+      e.printStackTrace()
+      system.terminate()
+  }
 
   Await.result(system.whenTerminated, Duration.Inf)
   //#http-server
