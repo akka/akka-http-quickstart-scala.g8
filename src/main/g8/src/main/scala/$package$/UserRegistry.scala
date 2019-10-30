048fe66f92742dacec612414ba8badf4a499eec4
Johan Andrén <johan@markatta.com>
2019-10-30 14:25:01 +0100

    Review feedback fix

--- src/main/g8/src/main/scala/$package$/UserRegistry.scala
+++ src/main/g8/src/main/scala/$package$/UserRegistry.scala
@@ -4,10 +4,11 @@ package $package$
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import scala.collection.immutable
 
 //#user-case-classes
 final case class User(name: String, age: Int, countryOfResidence: String)
-final case class Users(users: Seq[User])
+final case class Users(users: immutable.Seq[User])
 //#user-case-classes
 
 object UserRegistry {
