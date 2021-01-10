e22a8c7eb678db5a182fcb2dd95e8459753c8f94
Andrii <aabrasha@gmail.com>
2021-01-10 13:59:43 +0200

    Revert UserRoutesSpec to scaliest 3.1.4

--- src/main/g8/src/test/scala/$package$/UserRoutesSpec.scala
+++ src/main/g8/src/test/scala/$package$/UserRoutesSpec.scala
@@ -7,11 +7,10 @@ import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.{ Matchers, WordSpec }
 
 //#set-up
-class UserRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
+class UserRoutesSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
   //#test-top
 
   // the Akka HTTP route testkit does not yet support a typed actor system (https://github.com/akka/akka-http/issues/2036)
