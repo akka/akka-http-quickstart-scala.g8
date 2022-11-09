cb45430091711a69b78946f1e896dcc1d38af721
Johan Andrén <johan@markatta.com>
2022-11-09 17:00:46 +0100

    Ups

--- src/main/g8/src/main/scala/$package$/JsonFormats.scala
+++ src/main/g8/src/main/scala/$package$/JsonFormats.scala
@@ -11,7 +11,7 @@ object JsonFormats  {
   import DefaultJsonProtocol._
 
   implicit val userJsonFormat: RootJsonFormat[User] = jsonFormat3(User.apply)
-  implicit val usersJsonFormat: RootJsonFormat[User] = jsonFormat1(Users.apply)
+  implicit val usersJsonFormat: RootJsonFormat[Users] = jsonFormat1(Users.apply)
 
   implicit val actionPerformedJsonFormat: RootJsonFormat[ActionPerformed] = jsonFormat1(ActionPerformed.apply)
 }
